39e3cc55008cff86e46833628fe404f06037fdfa
Paul Thompson <pmt10@case.edu>
2022-04-15 14:49:17 -0400

    add button to indicate if camera is connected

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void drawConnected();
+    QGraphicsScene* on_button_scene;
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QGraphicsPixmapItem>
+#include <QPainterPath>
+#include <QPainter>
+#include <QGraphicsScene>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    on_button_scene = new QGraphicsScene(this);
+
+    QPainterPath on_button_path;
+    on_button_path.addEllipse(0.0,0.0,25.0,25.0);
+    on_button_path.setFillRule(Qt::WindingFill);
+
+    on_button_scene->addPath(on_button_path,QPen(Qt::red),QBrush(Qt::red));
+    ui->graphicsView_2->setScene(on_button_scene);
+    ui->graphicsView_2->show();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +28,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::drawConnected() {
+
+    on_button_scene->clear();
+
+    QPainterPath on_button_path;
+    on_button_path.addEllipse(0.0,0.0,25.0,25.0);
+    on_button_path.setFillRule(Qt::WindingFill);
+
+    on_button_scene->addPath(on_button_path,QPen(Qt::green),QBrush(Qt::green));
+}
